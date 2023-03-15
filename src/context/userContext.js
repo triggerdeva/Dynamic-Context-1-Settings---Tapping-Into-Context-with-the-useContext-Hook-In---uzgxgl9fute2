89c6a6c5fa6b891f7d98ae07ecc9ea6904bd65ae
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:25:11 +0530

    Update userContext.js

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,16 +1,14 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from 'react';
 
+const UserContext = createContext();
 
-const UserContext = createContext()
+const Wrapper = props => {
+	const [greeting, changeGreeting] = useState('Hello');
 
-const Wrapper = (props) => {
-
-    const greeting = "Hello"
-  
-    return (
-        <UserContext.Provider value={{ greeting }}>
-            {props.children}
-        </UserContext.Provider>
-    )
-}
-export { Wrapper, UserContext }
+	return (
+		<UserContext.Provider value={{ greeting, changeGreeting }}>
+			{props.children}
+		</UserContext.Provider>
+	);
+};
+export { Wrapper, UserContext };
