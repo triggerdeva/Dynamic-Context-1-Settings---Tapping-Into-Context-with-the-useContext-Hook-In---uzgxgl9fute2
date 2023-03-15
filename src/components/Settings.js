76ab4431ed8084ea73602cd0579e7b1717e1da5c
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 02:24:17 +0530

    Update Settings.js

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,16 +1,22 @@
-import React from "react"
-import { useContext } from "react"
-import { UserContext } from "../context/userContext"
+import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../context/userContext';
 
 export const Settings = () => {
+	// to be implemented in context
+	const { changeGreeting } = useContext(UserContext);
 
-    // to be implemented in context
-    const { changeGreeting } = useContext(UserContext)
-
-    return (
-        <div style={{ border: '5px solid red', padding: '8px' }} id="settings">
-            <h4>Settings</h4>
-            <input type={'text'} />
-        </div>
-    )
-}
+	return (
+		<div style={{ border: '5px solid red', padding: '8px' }} id="settings">
+			<h4>Settings</h4>
+			<input
+				type={'text'}
+				onChange={e => {
+					e.target.value
+						? changeGreeting(e.target.value)
+						: changeGreeting('Hello');
+				}}
+			/>
+		</div>
+	);
+};
